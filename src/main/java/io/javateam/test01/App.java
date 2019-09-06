145d635773926899ddd973cc89f38e7d1022c950
janusz.rachwol <janusz.rachwol@gmail.com>
2019-09-06 12:44:48 +0200

    pierwszy commit

---
+++ src/main/java/io/javateam/test01/App.java
@@ -0,0 +1,14 @@
+package io.javateam.test01;
+
+/**
+ * Hello world!
+ *
+ */
+public class App {
+	
+    public static void main( String[] args )  {
+    	
+    	FirstClass firstClass = new FirstClass ();
+    	firstClass.expe();
+    }
+}
---
+++ src/main/java/io/javateam/test01/FirstClass.java
@@ -0,0 +1,9 @@
+package io.javateam.test01;
+
+public class FirstClass {
+	
+	void expe () {
+		System.out.println("This is it");
+	}
+
+}
